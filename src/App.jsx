95736c3e02ef7294fd26ce4f93f3121d1a8244b9
Chigozirim Joshua <divinefavourjoshua03@gmail.com>
2025-03-27 11:52:41 +0100

    added new text

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ function App() {
 
   return (
     <>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, harum quas atque nisi nihil incidunt a nobis consequatur nam ipsam at obcaecati suscipit voluptatem aut libero, consequuntur esse sed velit.</p>
+      <h1>React App</h1>
+      <p>Testing</p>
     </>
   )
 }
