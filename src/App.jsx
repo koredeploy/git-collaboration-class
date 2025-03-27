f06090673e629a3a758acdad6c1f22c9c11961f9
olorunwa-web <olorunwamoliki@gmail.com>
2025-03-27 12:20:17 +0100

    resolve conflit

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ function App() {
     <p>Lorem ipsum dolor sit amet.</p>
     <p>Lorem ipsum dolor sit amet.</p>
     <p>Lorem ipsum dolor sit amet.</p>
+    <p>Lorem ipsum dolor sit amet.</p>
+
     </>
   )
 }
