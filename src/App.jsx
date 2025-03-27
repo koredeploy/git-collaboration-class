7230d2d0359078a7d5c624a3c553f4446f321347
Daniel <danielkore1234@gmail.com>
2025-03-27 11:18:58 +0100

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, harum quas atque nisi nihil incidunt a nobis consequatur nam ipsam at obcaecati suscipit voluptatem aut libero, consequuntur esse sed velit.</p>
+    </>
+  )
+}
+
+export default App
