3be3cbf60d7b9a10dcbdd682167c88b6e04f27cd
MojolaoluwaGafar <mojolaoluwa1212@gmail.com>
2025-03-27 11:52:01 +0100

    added label and input

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, harum quas atque nisi nihil incidunt a nobis consequatur nam ipsam at obcaecati suscipit voluptatem aut libero, consequuntur esse sed velit.</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, harum
+        quas atque nisi nihil incidunt a nobis consequatur nam ipsam at
+        obcaecati suscipit voluptatem aut libero, consequuntur esse sed velit.
+      </p>
+      <label>Name</label>
+      <input type="text" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
