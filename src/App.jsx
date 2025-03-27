ae68801e3e2fbff23772fbb644af9eb1c324c302
MojolaoluwaGafar <mojolaoluwa1212@gmail.com>
2025-03-27 12:23:39 +0100

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import "./App.css";
 function App() {
   return (
     <>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, harum quas atque nisi nihil incidunt a nobis consequatur nam ipsam at obcaecati suscipit voluptatem aut libero, consequuntur esse sed velit.</p>
-    <p>Lorem ipsum dolor sit amet.</p>
-    <p>Lorem ipsum dolor sit amet.</p>
-    <p>Lorem ipsum dolor sit amet.</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, harum
+        quas atque nisi nihil incidunt a nobis consequatur nam ipsam at
+        obcaecati suscipit voluptatem aut libero, consequuntur esse sed velit.
+      </p>
+      <p>Lorem ipsum dolor sit amet.</p>
+      <p>Lorem ipsum dolor sit amet.</p>
+      <p>Lorem ipsum dolor sit amet.</p>
     </>
   );
 }
